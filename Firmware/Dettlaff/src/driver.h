4d06a1e621814854e962b4fa0cb409f03e9de5a4
Jougheaux <jbrewster9894@gmail.com>
2023-08-13 15:58:55 -0400

    v0.3 files for JPEG

--- Firmware/Dettlaff/src/driver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-class Driver {
-public:
-    virtual void drive(float dutyCycle, bool reverseDirection) = 0;
-    virtual void brake() = 0;
-    virtual void coast() = 0;
-};
